272da9a9947de05406859a5d0fc43cf8f6b9d800
Belorina <ozcan_berrin@hotmail.com>
2021-10-24 18:48:05 +0200

    SecuMan with his newly behaviour script

---
+++ Assets/#Project/Scripts/SecuBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    // ---- Secu Check ----
+    // - can you pass? filter positive / negative 
+    // if yes - go 
+    // if no, stop poof 
+    
+public class SecuBehaviour : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
